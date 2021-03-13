c16888499268424c17b3db6cc6bc0d355b2d93c5
Talla Giridhar <giridhar.talla2002@gmail.com>
2021-03-13 09:01:34 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import './styles.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        Pomodoro Clock{' '}
+        <span role="img" aria-label="stopwatch">
+          ⏱
+        </span>
+      </h1>
+    </div>
+  )
+}
